ef669309636b90cb7765d3744985f400dc019521
Anatolii <anatorusan@gmail.com>
2023-03-08 19:44:31 +0200

    reverseMaxValue completed

---
+++ arrays/reversMaxValue.js
@@ -0,0 +1,5 @@
+const currentMaxValue = 4589;
+let reverseMaxValue = Number(String(currentMaxValue).split('').reverse().join(''));
+
+console.log(reverseMaxValue);
+console.log(typeof reverseMaxValue);
