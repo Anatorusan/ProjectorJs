60942ce0e9ecf26beedec4addaa97b70d99a256e
Anatolii <anatorusan@gmail.com>
2023-03-10 18:52:20 +0200

    updated product.js

--- arrays/product.js
+++ arrays/product.js
@@ -1,21 +1,5 @@
 'use strict'
 const resultsArray = [1, 2, [3, [4]]];
-
-function depthCounter(array) {
-    let counter = 0;
-    counterFunc(array);
-    function counterFunc(cascadedArray) {
-        for (let element of cascadedArray) {
-            if (Array.isArray(element)) {
-                counter ++;
-                counterFunc(element);
-            }
-        }
-        return
-    }
-    return counter;
-}
-
-const flattenArr = resultsArray.flat(depthCounter(resultsArray));
-let productOfArray = flattenArr.reduce((a, b) => a * b );
+const flattenArr = resultsArray.flat(Infinity);
+const productOfArray = flattenArr.reduce((a, b) => a * b );
 console.log(productOfArray);
