ce423fec916346418dcf1ac2809bdaa1ee5ecf26
Anatolii <anatorusan@gmail.com>
2023-03-10 19:02:49 +0200

    updated reversMaxValue.js

--- arrays/reversMaxValue.js
+++ arrays/reversMaxValue.js
@@ -1,6 +1,6 @@
 'use strict'
 const currentMaxValue = 4589;
-let reverseMaxValue = Number(String(currentMaxValue).split('').reverse().join(''));
+const reverseMaxValue = Number(String(currentMaxValue).split('').reverse().join(''));
 
 console.log(reverseMaxValue);
 console.log(typeof reverseMaxValue);
