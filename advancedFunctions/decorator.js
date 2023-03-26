a6e8859d86aea6385beb909729759548b82810c4
Anatolii <anatorusan@gmail.com>
2023-03-26 17:52:56 +0300

    added solution of decorator

---
+++ advancedFunctions/decorator.js
@@ -0,0 +1,18 @@
+const tribonacci = (signature,n) => {
+    const arr = signature;
+    for(let count = 3; count < n; count ++) {
+        arr.push(arr.slice(-3).reduce((a, b) => a + b));
+      }
+    arr.slice(0, n).forEach((element) => console.log(element));
+  }
+
+const slower = (func, seconds) => {
+  const wrapper = (...args) => {
+    console.log(`Chill out bro, you will get you result in ${seconds} seconds`);
+    return setTimeout(func, seconds*1000, ...args);
+  }
+  return wrapper;
+}
+
+const slowedTribonacci = slower(tribonacci, 5);
+slowedTribonacci([1, 1, 4], 15);
