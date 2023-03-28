0c56008a9490d33e44f791b5c390c9c9cf4ee05c
Anatolii <anatorusan@gmail.com>
2023-03-28 14:34:26 +0300

    updated byProperty.js

--- advancedFunctions/byProperty.js
+++ advancedFunctions/byProperty.js
@@ -24,35 +24,17 @@ const movies = [
     runningTimeInMinutes: 107,
     },
     ];
-  
-  
-  // const byProperty = (property, direction) => {
-  //   const modif = {
-  //     '>': 1,
-  //     '<': -1
-  //   }
-  //   const compareFunc = (a, b) => {
-  //     if (a[property] < b[property]) {
-  //       return -1 * modif[direction];
-  //     }
-  //     if (a[property] > b[property]) {
-  //       return 1 * modif[direction];
-  //     }
-  //     return 0;
-  //   }
-  //   return compareFunc;
-  // }
 
-  const byProperty = (property, direction) => {
-    const compareFunc = (a, b) => {
-      if (a[property] < b[property]) {
-        return direction === '>' ? -1 : 1;
+const byProperty = (property, direction) => {
+  const compareFunc = (a, b) => {
+    if (a[property] < b[property]) {
+      return direction === '>' ? -1 : 1;
     }
-      return direction === '>' ? 1 : -1;
+    return direction === '>' ? 1 : -1;
   }
-    return compareFunc;
-  }
-  // console.log(movies.sort(byProperty('releaseYear', '>')));
-  // console.log(movies.sort(byProperty('runningTimeInMinutes', '<')));
-  // console.log(movies.sort(byProperty('movieName', '>')));
-  // console.log(movies.sort(byProperty('directedBy', '>')));
+  return compareFunc;
+}
+// console.log(movies.sort(byProperty('releaseYear', '>')));
+// console.log(movies.sort(byProperty('runningTimeInMinutes', '<')));
+// console.log(movies.sort(byProperty('movieName', '>')));
+// console.log(movies.sort(byProperty('directedBy', '>')));
