11ed95c6cd50381e83e63a73c5907daef3123849
Anatolii <anatorusan@gmail.com>
2023-03-31 11:54:31 +0300

    completed the selectors task

---
+++ domFoundations/selectors/script.js
@@ -0,0 +1,11 @@
+//для елементу з текстом 'Навігація по DOM дереву'
+const domNavigation = document.getElementById('headerTwo');
+
+//для першого елементу <section>
+const sectionFirstChld = document.getElementsByTagName('section')[0].firstElementChild;
+
+//для елементу списку з текстом 'Пункт 5'
+const fifthListElem = document.querySelectorAll('li')[4];
+
+//для елементу з класом 'hatredLevelBlock'
+const hatredBlock = document.getElementsByClassName('hatredLevelBlock')[0];
