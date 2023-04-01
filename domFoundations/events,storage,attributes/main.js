38f626526565d490531d536b736f3ad8f5123a81
Anatolii <anatorusan@gmail.com>
2023-04-01 09:54:19 +0300

    refacrored trg, light and test switchers

--- domFoundations/events,storage,attributes/main.js
+++ domFoundations/events,storage,attributes/main.js
@@ -1,6 +1,8 @@
 const backgr = document.querySelector('body');
 const dateField = document.querySelector('.dateOutput');
 const swBtn = document.getElementById('swichButtn');
+const btnOnOff = document.querySelector('.btnOnOff');
+const dateOnOff = document.querySelector('.dateOnOff');
 
 const trigBox = {
     trigger: 1,
@@ -12,45 +14,76 @@ const trigBox = {
     }
 }
 
-const lightSwitcher = () => {
+const trigSwitcher = () => {
+    trigBox.trigger ? trigBox.trigOff() : trigBox.trigOn();
+    console.log(trigBox.trigger)
+}
 
-    const changer = () => {
-        if (trigBox.trigger) {
-            backgr.style.backgroundColor = '#282727';
-            dateField.style.color = '#ede5e5';
-            trigBox.trigOff();
-        } else {
-            backgr.style.backgroundColor = '';
-            dateField.style.color = '';
-            trigBox.trigOn();
-        }
-    }
-    return changer;
+const lightSwitcher = () => {
+    backgr.style.backgroundColor = trigBox.trigger ? '#282727' : '';
+    dateField.style.color = trigBox.trigger ? '#ede5e5' : '';
+}
+
+const textSwitcher = () => {
+    btnOnOff.innerHTML = trigBox.trigger ? 'On' : 'Off';
+    dateOnOff.innerHTML = !trigBox.trigger ? 'on' : 'off';
 }
 
 
 
-swBtn.addEventListener('click', lightSwitcher());
+swBtn.addEventListener('click', lightSwitcher);
+swBtn.addEventListener('click', textSwitcher);
+swBtn.addEventListener('click', trigSwitcher)
 
 
-// const box = document.querySelector("body > div")
-// box.style.backgroundColor = 'pink'
-// document.querySelector("body > div").style.backgroundColor = 'red';
 //alternative bckgr color: #282727
 //alternative date color: #ede5e5
 
-// const lightSwitcher = () => {
-//     let trigger = 1;
-//     const changer = () => {
-//         if (trigger) {
-//             backgr.style.backgroundColor = '#282727';
-//             dateField.style.color = '#ede5e5';
-//             trigger -= 1;
-//         } else {
-//             backgr.style.backgroundColor = '';
-//             dateField.style.color = '';
-//             trigger += 1;
-//         }
-//     }
-//     return changer;
-// }
+
+//exampl of startApp
+// const startApp = () => {
+//     console.log('Workshop1 startApp');
+
+//     const tasksList = loadList();
+
+//     renderList(tasksList);
+
+//     const updateList = () => {
+//         saveList(tasksList);
+//         renderList(tasksList);
+//     };
+
+//     const onSubmit = (newTask) => {
+//         tasksList.push({
+//             name: newTask,
+//             completed: false,
+//         });
+//         updateList();
+//     };
+
+//     formListener(onSubmit);
+
+//     listClickListener((index) => {
+//         tasksList.splice(index, 1);
+//         updateList();
+//     });
+
+//     formChangeListener((text) => {
+//         const filteredTasks = tasksList.filter(byName(text));
+//         renderList(filteredTasks);
+//     });
+// };
+
+// document.addEventListener('DOMContentLoaded', startApp);
+
+//exampe of local storage
+// const LS_KEY = 'tasksList';
+
+// export const saveList = (tasksList) => {
+//     localStorage.setItem(LS_KEY, JSON.stringify(tasksList));
+// };
+
+// export const loadList = (tasksList) => {
+//     const savedList = localStorage.getItem(LS_KEY);
+//     return savedList ? JSON.parse(savedList) : [];
+// };
