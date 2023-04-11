579a4e17d519694e2a26d334b0514c3f579c8b2b
Anatolii <anatorusan@gmail.com>
2023-04-11 22:56:52 +0300

    added minus month functionallity

--- courseProject/test.js
+++ courseProject/test.js
@@ -33,7 +33,7 @@ const initialDateLimiter = () => {
 }
 
 const calculate = () => {
-  if (endDateElement.value === '') {
+  if ((endDateElement.value === '') || (endDateElement.value < initialDateElement.value)) {
     return;
   }
   const initDateObj = new Date(initialDateElement.value);
@@ -47,35 +47,70 @@ const plusMinusBtnAtivator = () => {
 }
 
 const presetSwitcher = {
-  weekCounter: 1,
-  monthCounter: 1,
+  weekAddCounter: 1,
+  weekSubstrCounter: 1,
+  monthAddCounter: 1,
   
   addZero(stringToPad, targetLength = 2, padWithString = `0`) {
-         return `${stringToPad}`.padStart(targetLength, padWithString);
-    },
-  
-  addMonth() {
-    const baseDateObj = new Date(initialDateElement.value);
-    baseDateObj.setMonth(baseDateObj.getMonth() + this.monthCounter);
-    endDateElement.value = `${baseDateObj.getFullYear()}-${(this.addZero(baseDateObj.getMonth()+1))}-${this.addZero(baseDateObj.getDate())}`
-    console.log(this.monthCounter);
-    this.monthCounter += 1;
-    initialDateElement.setAttribute('max', endDateElement.value);
-    console.log('initial date limited')
-    },
+        return `${stringToPad}`.padStart(targetLength, padWithString);
+  },
 
-  resetCounter() {
-    console.log('reset counter called');
-    this.monthCounter = 1;
-    this.weekCounter = 1;
+  addMonth() {
+    console.log(`end date: ${endDateElement.value} start date: ${initialDateElement.value}`);
+    const baseDateObj = new Date(initialDateElement.value);
+    baseDateObj.setMonth(baseDateObj.getMonth() + this.monthAddCounter);
+    endDateElement.value = `${baseDateObj.getFullYear()}-${(this.addZero(baseDateObj.getMonth()+1))}-${this.addZero(baseDateObj.getDate())}`;
+    console.log(this.monthAddCounter);
+    this.monthAddCounter += 1;
+    initialDateElement.setAttribute('max', endDateElement.value);
+    console.log('initial date limited');
+    console.log(`end date: ${endDateElement.value} start date: ${initialDateElement.value}`);
+  },
+
+  substrMonth() {
+    console.log(`end date: ${endDateElement.value} start date: ${initialDateElement.value}`);
+    const startDateObj = new Date(initialDateElement.value);
+    const endDateObj = new Date(endDateElement.value);
+    if (endDateObj.getMonth() > startDateObj.getMonth()) {
+      endDateObj.setMonth(endDateObj.getMonth() - 1);
+      endDateElement.value = `${endDateObj.getFullYear()}-${(this.addZero(endDateObj.getMonth()+1))}-${this.addZero(endDateObj.getDate())}`;
+      initialDateElement.setAttribute('max', endDateElement.value);
+      return
     }
+    return
+  },
+
+  resetAllCounters() {
+    console.log('reset counter called');
+    this.monthAddCounter = 1;
+    this.weekAddCounter = 1;
+    this.monthAddCounter = 1;
+    this.monthSubstrCounter = 1;
+  },
+
+  resetAddCounters() {
+    this.monthAddCounter = 1;
+    this.weekAddCounter = 1;
+  },
+
+  // resetSubstrCounters() {
+  //   this.monthSubstrCounter = 1;
+  //   this.weekSubstrCounter = 1;
+  // }
 }
 
-const resetCounter = presetSwitcher.resetCounter.bind(presetSwitcher);
+const resetAllCounters = presetSwitcher.resetAllCounters.bind(presetSwitcher);
+
+const resetAddCounters = presetSwitcher.resetAddCounters.bind(presetSwitcher);
+
+// const resetSubstrCounters = presetSwitcher.resetSubstrCounters.bind(presetSwitcher);
 
 const addMonth = presetSwitcher.addMonth.bind(presetSwitcher);
 
-initialDateElement.addEventListener('input', resetCounter);
+const substrMonth = presetSwitcher.substrMonth.bind(presetSwitcher);
+
+
+initialDateElement.addEventListener('input', resetAllCounters);
 
 initialDateElement.addEventListener('input', endDateActivator);
 
@@ -86,3 +121,9 @@ endDateElement.addEventListener('change', initialDateLimiter);
 calcButtnElement.addEventListener('click', calculate);
 
 plusButtnElement.addEventListener('click', addMonth);
+
+// plusButtnElement.addEventListener('click', resetSubstrCounters);
+
+minusButtnElement.addEventListener('click', substrMonth);
+
+minusButtnElement.addEventListener('click', resetAddCounters);
